b365679d609aa18e3fe674586aec1f86cf5a6cc1
glaxyt <glaxyt@naver.com>
2024-08-14 02:19:09 +0900

    delete useless file

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,39 @@
+import 'package:ai_assistant/screen/SurveyPage.dart';
+import 'package:ai_assistant/screen/SurveyPage.dart';
+import 'package:ai_assistant/screen/splash_screen2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 import 'apis/app_write.dart';
 import 'helper/ad_helper.dart';
 import 'helper/global.dart';
 import 'helper/pref.dart';
-import 'screen/splash_screen.dart';
+
+// 전역 변수로 선언
+late InAppLocalhostServer localhostServer;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // init hive
+  // 로컬 서버 시작
+  localhostServer = InAppLocalhostServer();
+  await localhostServer.start();
+
+  // Init Hive (로컬 저장소)
+  await Hive.initFlutter();
+  await Hive.openBox('surveyBox');
+  await Hive.openBox('selectedServicesBox');
+
+  // Init preferences
   await Pref.initialize();
 
-  // for app write initialization
+  // For app write initialization
   AppWrite.init();
 
-  // for initializing facebook ads sdk
+  // For initializing Facebook ads SDK
   AdHelper.init();
 
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
@@ -35,10 +51,8 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: appName,
       debugShowCheckedModeBanner: false,
-
       themeMode: Pref.defaultTheme,
-
-      //dark
+      // Dark 모드일 때 기본 설정
       darkTheme: ThemeData(
           useMaterial3: false,
           brightness: Brightness.dark,
@@ -46,10 +60,9 @@ class MyApp extends StatelessWidget {
             elevation: 1,
             centerTitle: true,
             titleTextStyle:
-                TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
           )),
-
-      //light
+      // Light 모드일 때 기본 설정
       theme: ThemeData(
           useMaterial3: false,
           appBarTheme: const AppBarTheme(
@@ -60,19 +73,17 @@ class MyApp extends StatelessWidget {
             titleTextStyle: TextStyle(
                 color: Colors.blue, fontSize: 20, fontWeight: FontWeight.w500),
           )),
-
-      //
-      home: const SplashScreen(),
+      home: const SplashScreen2(), // SplashScreen2를 초기 화면으로 설정
     );
   }
 }
 
 extension AppTheme on ThemeData {
-  //light text color
+  // Light text color
   Color get lightTextColor =>
       brightness == Brightness.dark ? Colors.white70 : Colors.black54;
 
-  //button color
+  // Button color
   Color get buttonColor =>
       brightness == Brightness.dark ? Colors.cyan.withOpacity(.5) : Colors.blue;
 }
